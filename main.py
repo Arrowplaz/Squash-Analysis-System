81cae98f1e9ac50cd32563a13dd04ebbdbc84375
Abhi Reddy <anagireddygari@bowdoin.edu>
2024-10-15 21:05:42 -0400

    Addded boilerplate code to utilize keypoints model

--- main.py
+++ main.py
@@ -2,26 +2,45 @@ from utils import (read_video,
                    save_video
                    )
 
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
 
 def main():
+    print('Working')
     #Read in video
     input_video_path = './input_videos/rally.mp4'
     video_frames = read_video(input_video_path)
     
     #Detect players
     player_tracker = PlayerTracker('./models/yolov8x')
-    player_detections = player_tracker.detect_frames(video_frames)
+    player_detections = player_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path='./tracker_stubs/player_detections.pk1')
+
+    #Detect ball
+    ball_tracker = BallTracker('./models/ball_best.pt')
+    ball_detections = ball_tracker.detect_frames(video_frames,
+                                                 read_from_stub = True,
+                                                 stub_path ='./tracker_stubs/ball_detections.pk1')
+    
+    #Detect court keypoints
+    court_model_path = './models/keypoints_model.pth'
+    court_line_detector = CourtLineDetector(court_model_path)
+
+    #NOT WORKING
+    #court_keypoints = court_line_detector.predict(video_frames[0])
 
 
-    #Draw output
-
-    ##Draw Player Bounding Boxes
+    #Draw Player & Ball Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
+    #NOT WORKING
+    #Draw Court Keypoints
+    #output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     #Save the video
-    save_video(output_video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos2/output_video.avi')
 
 
 
--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,19 +1,31 @@
 from ultralytics import YOLO
 import cv2
+import pickle
 
 class PlayerTracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
 
 
-    def detect_frames(self, frames):
+    def detect_frames(self,frames, read_from_stub=False, stub_path=None):
         player_detections = []
-        for frame in frames:
-            player_detections.append(self.detect_frame(frame))
 
+        if read_from_stub and stub_path is not None:
+            with open(stub_path, 'rb') as f:
+                player_detections = pickle.load(f)
+            return player_detections
+
+        for frame in frames:
+            player_dict = self.detect_frame(frame)
+            player_detections.append(player_dict)
+        
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(player_detections, f)
+        
         return player_detections
     
-    def detect_frame(self, frame):
+    def detect_frame(self,frame):
         results = self.model.track(frame, persist=True)[0]
         id_name_dict = results.names
 
@@ -23,9 +35,8 @@ class PlayerTracker:
             result = box.xyxy.tolist()[0]
             object_cls_id = box.cls.tolist()[0]
             object_cls_name = id_name_dict[object_cls_id]
-            if object_cls_name == 'person':
+            if object_cls_name == "person":
                 player_dict[track_id] = result
-
         
         return player_dict
 

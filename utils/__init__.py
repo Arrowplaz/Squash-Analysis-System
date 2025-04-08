2ed4a53e7e2093a8fba5b93352eb4f92c0085fbb
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-04-08 11:47:41 -0400

    t

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,5 +4,5 @@ from .bbox_utils import measure_distance, get_center_of_bbox, get_foot_position,
 from .conversions import convert_meters_to_pixel_distance, convert_pixel_distance_to_meters
 from .manual_keypoints import get_user_selected_points
 from .score_detection import  get_user_selected_roi, detect_score, analyze_scoreboard, preprocess_scores
-from .heatmap import create_heatmap, map_detections, overlay_heatmap, compute_distance_traveled
+from .heatmap import create_heatmap, map_detections, overlay_heatmap, compute_distance_traveled, add_overlay
 from .mongoDB_utils import parse_file_name, insert_match, get_db

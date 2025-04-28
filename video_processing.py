4451500a7a0d7c1b54df9cdfc981739d96d81a7d
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-04-28 16:38:29 -0400

    t

--- video_processing.py
+++ video_processing.py
@@ -224,7 +224,7 @@ def process_videos_in_folder(folder_path):
 
 if __name__ == '__main__':
     input_folder = "./input_videos"
-    process_video("./input_videos/Alexander_Dartnell_V_Eiad_Daoud_#UK_Game2_Junior_M.mp4")
+    process_video("./input_videos/Tom_Walsh_V_Sanjay_Jeeva_#Canada_Game1_Pro_M.mp4")
     # process_videos_in_folder(input_folder)
     
     

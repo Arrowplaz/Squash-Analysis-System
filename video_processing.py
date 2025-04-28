75d59b6aeaaa116d3ac9fa923eec8eb1c79bcb79
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-04-28 12:06:48 -0400

    t

--- video_processing.py
+++ video_processing.py
@@ -64,8 +64,8 @@ def process_video(video_path):
     chunk_size = 1000  # Save every 1000 frames
     if os.listdir(detections_path) == []:
         while cap.isOpened():
-            # if frame_idx == 3000:
-            #     break
+            if frame_idx == 1000:
+                break
             ret, frame = cap.read()
             if not ret:
                 break

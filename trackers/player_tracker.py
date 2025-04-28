610cd9522d465117ccf2c041570a930c6971df1d
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-04-28 12:50:38 -0400

    t

--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -15,7 +15,7 @@ class PlayerTracker:
         self.main_ids = []
         self.previous_shirt_colors = {}
         self.color_history = {}
-        self.history_length = 15
+        self.history_length = 500
 
 
 

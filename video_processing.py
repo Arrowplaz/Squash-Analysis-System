56c05a736a1aa9e8d7bb94bdf55ac366402f18c1
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-04-28 21:52:40 -0400

    t

--- video_processing.py
+++ video_processing.py
@@ -78,7 +78,7 @@ def process_video(video_path):
             detections = player_tracker.detect_frame(frame)
             player_detections.append(detections)
             #filtered_detections = player_tracker.choose_and_filter_players(player_detections, court_keypoints)
-            player_detections = filtered_detections
+            # player_detections = filtered_detections
             output_frame = player_tracker.draw_bbox(frame, filtered_detections[-1])
             out.write(output_frame)  # Write frame directly to video
 
